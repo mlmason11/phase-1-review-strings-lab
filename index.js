b145d404f8b17ce83a735d6ad790855e1dcc7d61
Max <mlmason11@gmail.com>
2023-07-10 13:29:27 -0400

    Submitting strings lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = 'Max';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
